2040136bf9a4b7da29eb104b36fb9e83dd5e3508
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2021-01-02 13:59:46 +0300

    RemoteDesktopConnection

---
+++ UzakMasaustuBaglantisi/UzakMasaustuBaglantisi/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace UzakMasaustuBaglantisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMsTscAxNotSafeForScripting1 = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnconnect = new System.Windows.Forms.Button();
+            this.btndisconnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMsTscAxNotSafeForScripting1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMsTscAxNotSafeForScripting1
+            // 
+            this.axMsTscAxNotSafeForScripting1.Enabled = true;
+            this.axMsTscAxNotSafeForScripting1.Location = new System.Drawing.Point(12, 12);
+            this.axMsTscAxNotSafeForScripting1.Name = "axMsTscAxNotSafeForScripting1";
+            this.axMsTscAxNotSafeForScripting1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMsTscAxNotSafeForScripting1.OcxState")));
+            this.axMsTscAxNotSafeForScripting1.Size = new System.Drawing.Size(776, 369);
+            this.axMsTscAxNotSafeForScripting1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 408);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 415);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 415);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "USER:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(242, 408);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 411);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ŞİFRE:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(415, 408);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // btnconnect
+            // 
+            this.btnconnect.Location = new System.Drawing.Point(540, 405);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(94, 23);
+            this.btnconnect.TabIndex = 7;
+            this.btnconnect.Text = "BAĞLAN";
+            this.btnconnect.UseVisualStyleBackColor = true;
+            this.btnconnect.Click += new System.EventHandler(this.Btnconnect_Click);
+            // 
+            // btndisconnect
+            // 
+            this.btndisconnect.Location = new System.Drawing.Point(653, 405);
+            this.btndisconnect.Name = "btndisconnect";
+            this.btndisconnect.Size = new System.Drawing.Size(94, 23);
+            this.btndisconnect.TabIndex = 7;
+            this.btndisconnect.Text = "BAĞLANTI KES";
+            this.btndisconnect.UseVisualStyleBackColor = true;
+            this.btndisconnect.Click += new System.EventHandler(this.Btndisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btndisconnect);
+            this.Controls.Add(this.btnconnect);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.axMsTscAxNotSafeForScripting1);
+            this.Name = "Form1";
+            this.Text = "uzak masaüstü bağlantısı";
+            ((System.ComponentModel.ISupportInitialize)(this.axMsTscAxNotSafeForScripting1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsTscAxNotSafeForScripting axMsTscAxNotSafeForScripting1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnconnect;
+        private System.Windows.Forms.Button btndisconnect;
+    }
+}
+
---
+++ UzakMasaustuBaglantisi/UzakMasaustuBaglantisi/Form1.cs
@@ -0,0 +1,45 @@
+﻿using MSTSCLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UzakMasaustuBaglantisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnconnect_Click(object sender, EventArgs e)
+        {
+            axMsTscAxNotSafeForScripting1.Server = textBox1.Text;
+            axMsTscAxNotSafeForScripting1.UserName = textBox2.Text;
+            IMsTscNonScriptable guvenlik = (IMsTscNonScriptable)axMsTscAxNotSafeForScripting1.GetOcx();
+            guvenlik.ClearTextPassword = textBox3.Text;
+            axMsTscAxNotSafeForScripting1.Connect();
+            
+        }
+
+        private void Btndisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                axMsTscAxNotSafeForScripting1.Disconnect();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Zaten bağlı değil!");
+             //   throw;
+            }
+            
+        }
+    }
+}
